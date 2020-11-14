23e3b79dad33765d9b4557b38fefc691c44b41c6
julioUrjc <julio.martin.saez@gmail.com>
2020-11-14 10:40:42 +0100

    Preferences

--- MyFirstApplication2/app/src/main/java/com/july/myfirstapplication2/MainActivity.kt
+++ MyFirstApplication2/app/src/main/java/com/july/myfirstapplication2/MainActivity.kt
@@ -2,9 +2,14 @@ package com.july.myfirstapplication2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val key = "MY_KEY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
@@ -16,5 +21,40 @@ class MainActivity : AppCompatActivity() {
         //textView.text = "Cambiar el texto"
         //textView.text = getString(R.string.Hello)
 
+        //Obtenemos el PreferenceManager
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        // Recuperar las preferencias
+        getButton.setOnClickListener{
+            val myPref = prefs.getString(key,"No hay un valor para esta clave")
+            myPref?.let{ showAlert(myPref) }
+        }
+
+        // Guarda las preferencias
+        putButton.setOnClickListener{
+            //Modo edicion
+            val editor = prefs.edit()
+
+            editor.putString(key, "Mi valor")
+            editor.apply()
+            showAlert("Hemos guardado el valor $key")
+        }
+
+        // Borra las preferencias
+        delButton.setOnClickListener{
+            val editor = prefs.edit()
+
+            editor.remove(key)
+            editor.apply()
+            showAlert("Hemos borrado el valor $key")
+        }
+    }
+
+    private fun showAlert(message:String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("My preferences")
+        builder.setMessage(message)
+        val dialog = builder.create()
+        dialog.show()
     }
 }
