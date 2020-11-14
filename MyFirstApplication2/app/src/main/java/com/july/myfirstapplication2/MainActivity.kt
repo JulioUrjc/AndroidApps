808f4e95439a3f2be7129389823e58a378b170c2
julioUrjc <julio.martin.saez@gmail.com>
2020-11-14 09:57:24 +0100

    Idiomas

--- MyFirstApplication2/app/src/main/java/com/july/myfirstapplication2/MainActivity.kt
+++ MyFirstApplication2/app/src/main/java/com/july/myfirstapplication2/MainActivity.kt
@@ -2,14 +2,19 @@ package com.july.myfirstapplication2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        Thread.sleep(8000)
+        Thread.sleep(2000)
         setTheme(R.style.Theme_MyFirstApplication2)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //textView.text = "Cambiar el texto"
+        //textView.text = getString(R.string.Hello)
+
     }
 }
